266d4e5adf9bbd143bbe3813d5a16decbc9d402d
RACunderskrt <manuel.humbert@gmail.com>
2026-02-06 11:32:49 +0100

    fix inventory sql

--- DOCKER_GLOBAL/inventory-init/01-init-table.sql
+++ DOCKER_GLOBAL/inventory-init/01-init-table.sql
@@ -1,17 +1,18 @@
-CREATE TABLE inventory
-(
+CREATE TABLE inventory (
     id_hero INT NOT NULL,
     id_item INT NOT NULL,
-    amount INT NOT NULL
-    CONSTRAINT PK_inventory PRIMARY KEY (id_hero, id_item)
-)
+    amount INT NOT NULL,
+    CONSTRAINT pk_inventory PRIMARY KEY (id_hero, id_item)
+);
 
-
-CREATE TABLE item
-(
+CREATE TABLE item (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     usable BOOLEAN NOT NULL,
     quantity INT,
     unit INT
-)
+);
+
+ALTER TABLE inventory
+ADD CONSTRAINT fk_inventory_item
+FOREIGN KEY (id_item) REFERENCES item(id);
